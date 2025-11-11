bd5e42f9775f8f1b28a466f22c661326484d1c5c
Manikandan Kannan <manikandan.kannan@freelancers.outfittery.de>
2025-11-11 15:43:14 +0530

    revert changes

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,29 +1,8 @@
 import { AppProvider } from '@shopify/polaris';
-import { Provider as AppBridgeProvider, useAuthenticatedFetch } from '@shopify/app-bridge-react';
-import { ApolloProvider } from '@apollo/client';
 import RouteTeller from './routes';
 import { BrowserRouter } from 'react-router-dom';
-import { createApolloClient } from './apollo/client';
 import "@shopify/polaris/build/esm/styles.css"
 import en from "@shopify/polaris/locales/en.json";
-
-/**
- * Apollo Wrapper that uses App Bridge's authenticated fetch
- * This component must be inside AppBridgeProvider to access authenticatedFetch
- */
-function ApolloWrapper({ children }) {
-  // Get authenticated fetch from App Bridge - automatically includes session tokens
-  const authenticatedFetch = useAuthenticatedFetch();
-  
-  // Create Apollo Client with authenticated fetch
-  const apolloClient = createApolloClient(authenticatedFetch);
-
-  return (
-    <ApolloProvider client={apolloClient}>
-      {children}
-    </ApolloProvider>
-  );
-}
 
 function App() {
   const host = new URLSearchParams(window.location.search).get("host");
@@ -46,13 +25,9 @@ function App() {
 
   return (
     <BrowserRouter>
-      <AppBridgeProvider config={appBridgeConfig}>
-        <ApolloWrapper>
-          <AppProvider i18n={en}>
-            <RouteTeller />
-          </AppProvider>
-        </ApolloWrapper>
-      </AppBridgeProvider>
+      <AppProvider i18n={en}>
+        <RouteTeller />
+      </AppProvider>
     </BrowserRouter>
   );
 }
